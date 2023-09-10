bf4d329e2ed83768e41443d037e2b8c9be933af8
Ivan <ivanxu1116@gmail.com>
2023-09-10 00:17:01 -0700

    finished some generic framework stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/pixel_delivery/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/pixel_delivery/Intake.java
@@ -1,5 +1,22 @@
 package org.firstinspires.ftc.teamcode.robot.components.pixel_delivery;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 // todo needs renaming
 public class Intake {
+    DcMotor intakeMotor;
+    public Intake (HardwareMap hardwareMap, Telemetry telemetry) {
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        telemetry.update();
+    }
+    public void spinRight () {
+        intakeMotor.setPower(1.0);
+    }
+    public void spinLeft () {
+        intakeMotor.setPower(-1.0);
+    }
 }
