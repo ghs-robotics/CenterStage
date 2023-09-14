c5a9d2f07ddf803690025eefb2f9e4a3157d0fbb
zhaom <zhaomanyi2005@gmail.com>
2023-09-14 16:53:12 -0700

    Created and initialized gamepads in Tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Tele.java
@@ -4,14 +4,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.bot.Robot;
+import org.firstinspires.ftc.teamcode.opmodes.input.Controller;
 
 @TeleOp
 public class Tele extends LinearOpMode {
     Robot robot;
+    Controller gp1;
+    Controller gp2;
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap, telemetry);
+
+        gp1 = new Controller(gamepad1);
+        gp2 = new Controller(gamepad2);
 
         waitForStart();
         telemetry.addLine("Initializing");
@@ -22,7 +28,7 @@ public class Tele extends LinearOpMode {
             //-------------------------------------------------------------------------------------
             //                                  GAMEPAD 1
             //-------------------------------------------------------------------------------------
-            robot.drive.calculateDrivePowers(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+            robot.drive.calculateDrivePowers(gp1.left_stick_x, gp1.left_stick_y, gp1.right_stick_x);
 
 
 
