869140bb3226bf48fc433f585e9ced8bd7750bd4
Will Heath <76233664+wtheathOTG@users.noreply.github.com>
2023-09-14 17:58:55 -0700

    Starting Camera Code and copied color detection from 2023 code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/Camera.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode.cv;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
 public class Camera {
+
+    public Telemetry telemetry;
+
+    public OpenCvCamera cam;
+
+    public TeamPropPipeline TPPipeline;
+
+    public Camera(HardwareMap hardwareMap){
+        cam = OpenCvCameraFactory.getInstance()
+                .createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
+
+        TPPipeline = new TeamPropPipeline();
+    }
+
+    public void init(){
+        cam.setPipeline(TPPipeline);
+        cam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                cam.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addLine("Error: Camera could not open");
+                telemetry.update();
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/TeamPropPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/TeamPropPipeline.java
@@ -1,11 +1,93 @@
 package org.firstinspires.ftc.teamcode.cv;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
+import java.util.ArrayList;
+
 public class TeamPropPipeline extends OpenCvPipeline {
+    public TeamPropPipeline(){
+
+    }
+
     @Override
     public Mat processFrame(Mat input) {
+        //Crop image
+        //Rect rectCrop = new Rect((int) (input.width() / 2), (int) (input.height() / 2), (int) (input.width() / 5), (int) (input.height() / 5));
+        //input = new Mat(input, rectCrop);
+
+        //Sample Center Pixels
+        //Rect sampleCrop = new Rect((int) (input.width() / 2), (int) (input.height() / 2), 15, 15);
+        //Mat colorSample = new Mat(input, sampleCrop);
+
+        //convert image from RGB to HSV
+        /*Mat hsvConvert = new Mat();
+        cvtColor(colorSample, hsvConvert, Imgproc.COLOR_RGB2HSV);
+
+        //Find average of each channel across sample pixels
+        Mat avgColColor = new Mat();
+        Mat avgColor = new Mat();
+        Core.reduce(hsvConvert, avgColColor, 0, Core.REDUCE_AVG);
+        Core.reduce(avgColColor, avgColor, 1, Core.REDUCE_AVG);
+        ArrayList<Mat> channels = new ArrayList<Mat>(3);
+        Core.split(avgColor, channels);
+        Mat hueChannel = channels.get(0);
+
+        hueTargets = new ArrayList<Double>();
+        hueTargets.add(38.0);  //[ 38  ]
+        hueTargets.add(156.0); //[ 156 ]
+        hueTargets.add(95.0); //[ 195 ]
+
+        //Compute distance from each target color hue
+        ArrayList<Double> hueDiff = new ArrayList<Double>();
+//
+        for(int i=0; i<hueTargets.size(); i++) {
+            hueDiff.add(Math.abs(hueTargets.get(i) - hueChannel.get(0, 0)[0]));
+        }
+
+//        Core.absdiff(hueTargets, hueChannel, hueDiff);
+//
+//        //Find location of min hue difference
+        int result = -1;
+        int minIndex = 2;
+        double min = hueDiff.get(2);
+        for(int i=1; i>=0; i--) {
+            if(hueDiff.get(i) < min) {
+                minIndex = i;
+                min = hueDiff.get(i);
+            }
+        }
+
+//        telemetry.addLine("Sighted Hue: " + hueChannel.get(0, 0)[0]);
+//        telemetry.addLine("Hue Difference: " + hueDiff.get(minIndex));
+//        telemetry.addLine("Result is: " + result);
+//        telemetry.addLine("MinIndex is: " + minIndex);
+//        telemetry.addLine("" + hueDiff.get(0));
+//        telemetry.addLine("" + hueDiff.get(1));
+//        telemetry.addLine("" + hueDiff.get(2));
+//
+//        telemetry.update();
+
+        //Add color detection hue diff is small enough and not already detected
+        if(hueDiff.get(minIndex) < hueThreshold ) {
+            result = minIndex;
+            boolean included = false;
+            for (int detection : colorDetections)
+                if (result == detection) included = true;
+            if(!included) colorDetections.add(new Integer(result));
+            //colorDetections.remove(0);
+        }
+
+        //Draw rectangle around center 30x30 pixels to help line up cameraq
+
+        Point upperLeft = new Point((int)input.cols()/2 - 10, (int)input.cols()/2 + 10);
+        Point bottomRight = new Point((int)input.cols()/2 + 10, (int)input.cols()/2 - 10);
+        rectangle(input, upperLeft, bottomRight, new Scalar(255, 25, 25), 2);
+
+        return input;*/
         return null;
     }
 }
