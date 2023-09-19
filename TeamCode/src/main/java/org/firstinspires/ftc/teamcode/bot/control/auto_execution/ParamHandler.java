b6549003a813ee739f41cd5c0815405eca8293d5
zhaom <zhaomanyi2005@gmail.com>
2023-09-19 16:40:17 -0700

    fixed auto error issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/control/auto_execution/ParamHandler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/control/auto_execution/ParamHandler.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.teamcode.bot.control.auto_execution;
 
+import java.sql.ParameterMetaData;
+
 public class ParamHandler {
     int x;
     int y;
     double heading;
+
+    public ParamHandler(){
+
+    }
 
     public ParamHandler(int action, int x, int y, double heading){
         this.x = x;
