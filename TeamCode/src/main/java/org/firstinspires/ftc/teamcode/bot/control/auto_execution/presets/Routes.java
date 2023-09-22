823f55b1f868a4c1db2092ba8c31e0760fe86126
zhaom <zhaomanyi2005@gmail.com>
2023-09-22 02:38:29 -0700

    beginning set up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/control/auto_execution/presets/Routes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/control/auto_execution/presets/Routes.java
@@ -1,4 +1,10 @@
 package org.firstinspires.ftc.teamcode.bot.control.auto_execution.presets;
 
+import org.firstinspires.ftc.teamcode.bot.control.auto_execution.AutoActionHandler;
+import org.firstinspires.ftc.teamcode.bot.control.auto_execution.AutoActions;
+
+import java.util.ArrayList;
+
 public class Routes {
+    public AutoActionHandler routeA;
 }
