388e0316d56ade7d934a9dddda8d7a6b7e4ccc4d
Will Heath <76233664+wtheathOTG@users.noreply.github.com>
2023-10-10 17:39:31 -0700

    Implemented Theoretically functioning spike Mark detection?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/TeamPropPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/TeamPropPipeline.java
@@ -26,26 +26,121 @@ public class TeamPropPipeline extends OpenCvPipeline {
 
     }
 
-    @Override
-    public Mat processFrame(Mat input) {
-        //Crop image
-        //Rect rectCrop = new Rect((int) (input.width() / 2), (int) (input.height() / 2), (int) (input.width() / 5), (int) (input.height() / 5));
-        //input = new Mat(input, rectCrop);
+    public Mat processFrame(Mat input){
+        return input;
+    }
 
-        //Sample Center Pixels
-        //Rect sampleCrop = new Rect((int) (input.width() / 2), (int) (input.height() / 2), 15, 15);
-        //Mat colorSample = new Mat(input, sampleCrop);
+    public int processImage(Mat input) {
+        //Crop mat
+        int leftCrop = (input.width() / 2) - 250;
+        int rightCrop = (input.width() / 2) + 250;
 
-        //convert image from RGB to HSV
-        Mat hsvConvert = new Mat();
-        Imgproc.cvtColor(input, hsvConvert, Imgproc.COLOR_RGB2HSV);
+        //Create left mat
+        Rect rectLeft = new Rect(
+                0,
+                0,
+                leftCrop - 1,
+                input.height()
+        );
+        Mat leftMat = new Mat(input, rectLeft);
 
+        //Create middle mat
+        Rect rectMiddle = new Rect(
+                leftCrop,
+                0,
+                rightCrop - leftCrop,
+                input.height()
+        );
+        Mat middleMat = new Mat(input, rectMiddle);
+
+        //Create right mat
+        Rect rectRight = new Rect(
+                rightCrop + 1,
+                0,
+                input.width() - (rightCrop + 1),
+                input.height()
+        );
+        Mat rightMat = new Mat(input, rectRight);
+
+
+
+        //convert all mats from RGB to HSV
+        Mat leftMatHSV = new Mat();
+        Imgproc.cvtColor(leftMat, leftMatHSV, Imgproc.COLOR_RGB2HSV);
+
+        Mat middleMatHSV = new Mat();
+        Imgproc.cvtColor(middleMat, middleMatHSV, Imgproc.COLOR_RGB2HSV);
+
+        Mat rightMatHSV = new Mat();
+        Imgproc.cvtColor(rightMat, rightMatHSV, Imgproc.COLOR_RGB2HSV);
+
+
+
+        //Get all mats hues in threshold
+        int leftInThreshold = 0;
+        int middleInThreshold = 0;
+        int rightInThreshold = 0;
+
+        int targetHue = 0;
+        int hueThreshold = 15;
+
+        int pixelX;
+        int pixelY;
+
+        //Left hues in Threshold
+        int matWidth = leftMatHSV.width();
+        int matHeight = leftMatHSV.height();
+
+        for (pixelX = 0; pixelX < matWidth; pixelX++){
+            for (pixelY = 0; pixelY < matHeight; pixelY++){
+                int hue = (int) leftMatHSV.get(pixelX, pixelY)[0];
+                if (Math.abs((targetHue - hue + 540) % 360 - 180) <= hueThreshold ) leftInThreshold++;
+            }
+        }
+
+        //Middle hues in Threshold
+        matWidth = middleMatHSV.width();
+        matHeight = middleMatHSV.height();
+
+        for (pixelX = 0; pixelX < matWidth; pixelX++){
+            for (pixelY = 0; pixelY < matHeight; pixelY++){
+                int hue = (int) middleMatHSV.get(pixelX, pixelY)[0];
+                if (Math.abs((targetHue - hue + 540) % 360 - 180) <= hueThreshold ) middleInThreshold++;
+            }
+        }
+
+        //Right hues in Threshold
+        matWidth = rightMatHSV.width();
+        matHeight = rightMatHSV.height();
+
+        for (pixelX = 0; pixelX < matWidth; pixelX++){
+            for (pixelY = 0; pixelY < matHeight; pixelY++){
+                int hue = (int) rightMatHSV.get(pixelX, pixelY)[0];
+                if (Math.abs((targetHue - hue + 540) % 360 - 180) <= hueThreshold ) rightInThreshold++;
+            }
+        }
+
+        //0 = left, 1 = middle, 2 = right
+        int spikeMark;
+        //Check greatest hues in Threshold
+        if (leftInThreshold > rightInThreshold){
+            if (leftInThreshold > middleInThreshold) spikeMark = 0;
+            else spikeMark = 1;
+        } else if (rightInThreshold > middleInThreshold) spikeMark = 2;
+        else spikeMark = 1;
+
+
+        return spikeMark;
+
+
+
+        /*
         //Find average color of pixels
         Mat avgColorColumn = new Mat();
         Mat avgColor = new Mat();
 
         //Find average of each column
-        Core.reduce(hsvConvert, avgColorColumn, 0, Core.REDUCE_AVG);
+        Core.reduce(leftMatHSV, avgColorColumn, 0, Core.REDUCE_AVG);
         //Find average
         Core.reduce(avgColorColumn, avgColor, 1, Core.REDUCE_AVG);
 
@@ -99,12 +194,10 @@ public class TeamPropPipeline extends OpenCvPipeline {
             //colorDetections.remove(0);
         }
 
-        /*//Draw rectangle around center 30x30 pixels to help line up camera
+        //Draw rectangle around center 30x30 pixels to help line up camera
         Point upperLeft = new Point((int)input.cols()/2 - 10, (int)input.cols()/2 + 10);
         Point bottomRight = new Point((int)input.cols()/2 + 10, (int)input.cols()/2 - 10);
-        rectangle(input, upperLeft, bottomRight, new Scalar(255, 25, 25), 2);*/
-
-        return input;
-        //return null;
+        rectangle(input, upperLeft, bottomRight, new Scalar(255, 25, 25), 2);
+        */
     }
 }
