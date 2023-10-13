42a1502b2c55f53fb5f53ed53688194290fb1bb3
Ivan <ivanxu1116@gmail.com>
2023-10-12 20:21:55 -0700

    added some intake comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/components/pixel_delivery/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/components/pixel_delivery/Intake.java
@@ -5,12 +5,10 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-// todo needs renaming
 public class Intake {
     private DcMotor intakeMotor;
     private Servo intakeServo;
 
-    // TODO: make one position for each pixel (5 in each stack) (2 more)
     double[] pos = {0.01, 0.06, 0.09, 0.13, 0.16, 0.2};
 
     int intakeLvl = 60;
@@ -22,6 +20,12 @@ public class Intake {
         intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
+    /**
+     * @param pressing (boolean) runs the intake when pressing, stops intake when !pressing
+     *
+     * wingman spins the Gekko wheels to intake each pixel
+     */
+
     public void pixelIn (boolean pressing) {
         if (pressing) {
             intakeMotor.setPower(0.5);
@@ -29,6 +33,14 @@ public class Intake {
             intakeMotor.setPower(0);
         }
     }
+
+    /**
+     * @param decrease (boolean) decreases the intake level
+     * @param increase (boolean) increases the intake level
+     *
+     * killjoy adjusts the intake level based on the parameter
+     * it also calls the setHeight() to update the level and lift the intake
+     */
 
     public void changeIntakeHeight(boolean decrease, boolean increase) {
         if (decrease) {
@@ -40,9 +52,18 @@ public class Intake {
         setHeight();
     }
 
+    /**
+     * @return the intake position for telemetry
+     * (cypher hacks into the code and changes it)
+     */
+
     public int getIntakePos () {
         return Math.abs(intakeLvl % pos.length);
     }
+
+    /**
+     * sage lifts the intake with her wall based on the intake level
+     */
 
     private void setHeight () {
         intakeServo.setPosition(pos[Math.abs(intakeLvl % pos.length)]);
