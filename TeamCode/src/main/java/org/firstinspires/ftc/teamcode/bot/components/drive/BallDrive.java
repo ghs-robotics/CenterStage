5dfa038d6963451efa0a46eb1bf72e43fe818c6d
manyi <zhaomanyi2005@gmail.com>
2024-01-07 15:45:46 -0800

    simple odometry code in balldrive class, if it works we can cut down on some classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/components/drive/BallDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/components/drive/BallDrive.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode.bot.components.drive;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.bot.components.Gyro;
-import org.opencv.core.Mat;
 
 public class BallDrive {
     private DcMotor leftDrive;
@@ -26,12 +24,13 @@ public class BallDrive {
     private double y;
     private double heading;
 
-    private int lastL;
-    private int lastR;
-    private int lastB;
+    private double lastL, lastR, lastB;
+    private double left, right, back;
+    private double deltaL, deltaR, deltaB, deltaH;
     private double lastH;
 
-    ElapsedTime timer;
+    private PID xPID;
+    private PID yPID;
 
     public double errorX;
 
@@ -49,19 +48,36 @@ public class BallDrive {
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        xPID = new PID();
+        yPID = new PID();
+
         lastL = 0;
         lastR = 0;
         lastB = 0;
 
-        timer = new ElapsedTime();
-
         this.gyro = gyro;
+    }
+
+    public boolean runToPosition(double x, double y){
+        xPID.setTarget(x);
+        yPID.setTarget(y);
+
+        double xPower = xPID.getOutput(x);
+        double yPower = yPID.getOutput(y);
+
+        calculateDrivePowers(xPower, yPower, 0, true);
+
+        return xPID.getError() + yPID.getError() < 100;
     }
 
 
     public void update(){
-        x = backDrive.getCurrentPosition();
-        y = .5 * (leftDrive.getCurrentPosition() + rightDrive.getCurrentPosition());
+        updateValues();
+
+        double arc = (deltaR + deltaL) * .5;
+
+        x += arc * Math.cos(heading + deltaH *.5);
+        y += arc * Math.sin(heading + deltaH *.5);
     }
 
     public void calculateDrivePowers(double x, double y, double rot) {
@@ -73,15 +89,14 @@ public class BallDrive {
     }
 
     public void calculateDrivePowers(double x, double y, double rot, boolean driveMode){
-        double angle = heading;
         metaDriveOn = driveMode;
 
         double driveX = x;
         double driveY = y;
 
         if(driveMode) {
-            driveX = x * Math.cos(angle) - y * Math.sin(angle) ;
-            driveY = y * Math.cos(angle) + x * Math.sin(angle);
+            driveX = x * Math.cos(heading) - y * Math.sin(heading) ;
+            driveY = y * Math.cos(heading) + x * Math.sin(heading);
         }
         calculateDrivePowers(driveX, driveY, rot);
     }
@@ -108,7 +123,6 @@ public class BallDrive {
 
     public void resetCoords(){
         resetEncoders();
-        timer.reset();
         x = 0;
         y = 0;
         heading = 0;
@@ -124,5 +138,88 @@ public class BallDrive {
 
     public double getHeading() {
         return Math.toDegrees(heading);
+    }
+
+    private void updateValues(){
+        back = backDrive.getCurrentPosition();
+        left = leftDrive.getCurrentPosition();
+        right = rightDrive.getCurrentPosition();
+        heading = gyro.getHeading();
+
+        deltaB = back - lastB;
+        deltaL = left - lastL;
+        deltaR = right - lastR;
+        deltaH = heading - lastH;
+
+        lastB = back;
+        lastL = left;
+        lastR = right;
+        lastH = heading;
+    }
+
+    private class PID{
+        private double lastError;
+        private double lastIntegral;
+
+        private double error;
+        private double integral;
+        private double derivative;
+
+        private double kp = .6;
+        private double ki = 1.2;
+        private double kd = 0.05;
+        private double bias = 0;
+
+        private double target;
+
+        private ElapsedTime timer;
+
+        private double lastIterationTime;
+
+        PID(double p, double i, double d){
+            this();
+            kp = p;
+            ki = i;
+            kd = d;
+        }
+
+        PID(){
+            timer = new ElapsedTime();
+        }
+
+        double getOutput(double current){
+            double iterationTime = timer.milliseconds() - lastIterationTime;
+            lastIterationTime += iterationTime;
+
+            error = target - current;
+            integral = lastIntegral + error * iterationTime;
+            derivative = (error - lastError) / iterationTime;
+
+            double out = kp * error + ki * integral + kd * derivative + bias;
+
+            lastError = error;
+            lastIntegral = integral;
+
+            return out;
+        }
+
+        double getError(){
+            return error;
+        }
+
+        void setTarget(double target){
+            if (this.target == target)
+                return;
+
+            this.target = target;
+            lastError = 0;
+            lastIntegral = 0;
+            error = 0;
+            integral = 0;
+            derivative = 0;
+            timer.reset();
+            lastIterationTime = 0;
+        }
+
     }
 }
