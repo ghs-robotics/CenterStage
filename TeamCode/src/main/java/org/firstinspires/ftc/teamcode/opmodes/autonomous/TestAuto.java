f19f1847acb36d245bb14d6e9e37fcf27fbab49d
zhaom <zhaomanyi2005@gmail.com>
2024-01-13 00:13:28 -0800

    setting up run lift to position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/TestAuto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
+import static org.firstinspires.ftc.teamcode.bot.control.auto_execution.AutoActions.LIFT;
 import static org.firstinspires.ftc.teamcode.bot.control.auto_execution.AutoActions.MOVE;
 import static org.firstinspires.ftc.teamcode.bot.control.auto_execution.AutoActions.WAIT;
 
@@ -27,15 +28,7 @@ public class TestAuto extends LinearOpMode {
 //        actionHandler.add(MOVE, 50, 50, 0.0);
 //        actionHandler.add(MOVE, 0, 50, 0.0);
 //        actionHandler.add(MOVE, 0, 0, 0.0);
-        actionHandler.add(MOVE, 250, 0, 0.0);
-        actionHandler.add(WAIT, 0.01);
-        actionHandler.add(MOVE, 50, 0, 0.0);
-        actionHandler.add(WAIT, 1);
-        actionHandler.add(MOVE, 0, 0, 0.0);
-        actionHandler.add(WAIT, 3);
-        actionHandler.add(MOVE, 0, 200, 0.0);
-        actionHandler.add(WAIT, 3);
-        actionHandler.add(MOVE, 0, 0, 0.0);
+        actionHandler.add(LIFT, 250);
 //        actionHandler.add(WAIT, 20);
 
 
