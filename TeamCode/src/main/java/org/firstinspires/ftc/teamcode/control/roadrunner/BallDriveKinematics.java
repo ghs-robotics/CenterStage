5691f6b1a1bdef238f8099f64d9e13a3218dfa28
manyi <zhaomanyi2005@gmail.com>
2024-01-16 10:46:54 -0800

    trying to implement roadrunner and integrate it with balldrive :c

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/roadrunner/BallDriveKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/roadrunner/BallDriveKinematics.java
@@ -1,13 +1,27 @@
 package org.firstinspires.ftc.teamcode.control.roadrunner;
 
-public class BallDriveKinematics {
+import com.acmerobotics.roadrunner.DualNum;
+import com.acmerobotics.roadrunner.MecanumKinematics;
+import com.acmerobotics.roadrunner.TankKinematics;
+
+public class BallDriveKinematics{
     private double trackWidth;
     private double lateralMultiplier;
+
+    DualNum<Localizer.Params>[] wheelIncrements;
+
+    DualNum<Localizer.Params> left;
+    DualNum<Localizer.Params> right;
+    DualNum<Localizer.Params> back;
+
 
     public BallDriveKinematics(double trackWidth, double wheelBase, double lateralMultiplier){
         this.trackWidth = (trackWidth + wheelBase) / 2;
         this.lateralMultiplier = lateralMultiplier;
+
+        wheelIncrements = new DualNum[]{left, right, back};
     }
 
-//    public class WheelIncrements<Param>
+    public
+
 }
