d6d20dbbcad22d497711ce0432c86deb7e27509f
manyi <zhaomanyi2005@gmail.com>
2024-01-17 00:36:14 -0800

    still translating

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/roadrunner/BallDriveKinematics.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/roadrunner/BallDriveKinematics.java
@@ -1,27 +1,38 @@
 package org.firstinspires.ftc.teamcode.control.roadrunner;
 
+import static org.firstinspires.ftc.teamcode.control.roadrunner.Localizer.*;
+
 import com.acmerobotics.roadrunner.DualNum;
-import com.acmerobotics.roadrunner.MecanumKinematics;
-import com.acmerobotics.roadrunner.TankKinematics;
+import com.acmerobotics.roadrunner.Twist2dDual;
+import com.acmerobotics.roadrunner.Vector2dDual;
 
 public class BallDriveKinematics{
     private double trackWidth;
     private double lateralMultiplier;
 
-    DualNum<Localizer.Params>[] wheelIncrements;
+    DualNum[] ballIncrements;
 
-    DualNum<Localizer.Params> left;
-    DualNum<Localizer.Params> right;
-    DualNum<Localizer.Params> back;
+    DualNum<Params> left;
+    DualNum<Params> right;
+    DualNum<Params> back;
 
 
     public BallDriveKinematics(double trackWidth, double wheelBase, double lateralMultiplier){
         this.trackWidth = (trackWidth + wheelBase) / 2;
         this.lateralMultiplier = lateralMultiplier;
 
-        wheelIncrements = new DualNum[]{left, right, back};
+        ballIncrements = new DualNum[]{left, right, back};
     }
 
-    public
+    /**
+     * @param motors use wheelIncrements from this class
+     * @return adjusted powers
+     */
+    public Twist2dDual<Params> forward(DualNum<Params>[] motors){
+        Twist2dDual<Params> powers = new Twist2dDual<Params>(
+                new Vector2dDual<Params>(back, (left + right) * 0.5, );
+
+
+    }
 
 }
